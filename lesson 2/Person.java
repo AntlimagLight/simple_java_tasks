16166d2ff1442448b68866e56352fbecdde71606
Антон <antlighter@gmail.com>
2022-07-25 13:41:23 +0300

    Первое задание урока 2 + Правки по дз урока 1

---
+++ lesson 2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String name = "Василий";
+    String gender = "Мужской";
+    int age = 30;
+    int weight = 60;
+    int height = 180;
+
+    public void go() {
+        System.out.println("Человек идет");
+    }
+
+    public void sit() {
+        System.out.println("Человек сидит");
+    }
+
+    public void run() {
+        System.out.println("Человек бежит");
+    }
+
+    public void learnJava() {
+        System.out.println("Человек учит Java");
+    }
+
+    public void say() {
+        System.out.println("Человек говорит");
+    }
+
+}
