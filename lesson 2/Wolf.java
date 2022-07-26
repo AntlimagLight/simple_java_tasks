2b7f2abd8c6ab512a625c3ed5791b7d7e51ad979
Антон <antlighter@gmail.com>
2022-07-26 09:54:08 +0300

    Модифицированы классы "Wolf" и "WolfTest" в соответствии с домашним заданием по Инкапсуляции

--- lesson 2/Wolf.java
+++ lesson 2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-    String name;
-    String gender;
-    String color;
-    int age;
-    int weight;
+    private String name;
+    private String gender;
+    private String color;
+    private int age;
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println(" Внимание! Некорректный возраст \n");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
     public void go() {
         System.out.println(" Волк идет");
--- lesson 2/WolfTest.java
+++ lesson 2/WolfTest.java
@@ -1,13 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf reks = new Wolf();
-        reks.name = "Рекс";
-        reks.gender = "Самец";
-        reks.color = "Серый";
-        reks.age = 5;
-        reks.weight = 40;
-        System.out.println(" Создан новый волк! \n Кличка:" + reks.name + "\n Пол:" + reks.gender +
-                "\n Окрас:" +  reks.color + "\n Возраст:" + reks.color + "\n Вес:" + reks.color );
+        reks.setName("Рекс");
+        reks.setGender("Самец");
+        reks.setColor("Серый");
+        reks.setAge(5);
+        reks.setWeight(40);
+        System.out.println(" Создан новый волк! \n Кличка:" + reks.getName() + "\n Пол:" +
+                reks.getGender() + "\n Окрас:" +  reks.getColor() + "\n Возраст:" + reks.getAge() +
+                "\n Вес:" + reks.getWeight() + "\n");
         reks.go();
         reks.sit();
         reks.run();
