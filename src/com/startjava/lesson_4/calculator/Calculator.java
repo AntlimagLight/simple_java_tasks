6646fc132838be926bd57b80870c302e141d982e
LightAntimag <antlighter@gmail.com>
2022-08-15 15:26:38 +0300

    Переделал в соответствии с замечаниями

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -1,32 +1,26 @@
 package com.startjava.lesson_4.calculator;
 
 public class Calculator {
-
-    private static int num1;
-    private static int num2;
-    private static char sign;
-
-    public static void setNum1(int num1) {
-        Calculator.num1 = num1;
-    }
-
-    public static void setNum2(int num2) {
-        Calculator.num2 = num2;
-    }
-
-    public static void setSign(char sign) {
-        Calculator.sign = sign;
-    }
-
-    public static int calculate() {
+    public static int calculate(String mathAction) throws InvalidValuesException {
+        String[] mathElements = mathAction.split(" ");
+        float num1 = Float.parseFloat(mathElements[0]);
+        float num2 = Float.parseFloat(mathElements[2]);
+        char sign = mathElements[1].charAt(0);
+        if (!(checkIntPosNum(num1) && checkIntPosNum(num2))) {
+            throw new InvalidValuesException();
+        }
         return switch (sign) {
-            case '+' -> Math.addExact(num1, num2);
-            case '-' -> Math.subtractExact(num1, num2);
-            case '*' -> Math.multiplyExact(num1, num2);
-            case '/' -> (num1 / num2);
-            case '^' -> (int) Math.pow(num1, num2);
+            case '+' -> Math.addExact((int) num1, (int) num2);
+            case '-' -> Math.subtractExact((int) num1, (int) num2);
+            case '*' -> Math.multiplyExact((int) num1, (int) num2);
+            case '/' -> ((int) num1 / (int) num2);
+            case '^' -> (int) Math.pow((int) num1, (int) num2);
             default -> throw new IllegalArgumentException("Недопустимая операция!");
         };
     }
+    private static boolean checkIntPosNum(float num) {
+        return (num % 1 == 0 && num >= 0);
+    }
 }
 
+class InvalidValuesException extends Exception {}
--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -8,30 +8,21 @@ public class CalculatorTest {
         String option = "yes";
         while (option.equals("yes")) {
             Scanner scanner = new Scanner(System.in);
-            float num1 = 0;
-            float num2 = 0;
             try {
-                do {
-                    System.out.println("Введите математическое выражение: ");
-                    String mathAction = scanner.nextLine();
-                    String[] mathElements = mathAction.split(" ");
-                    num1 = Float.parseFloat(mathElements[0]);
-                    num2 = Float.parseFloat(mathElements[2]);
-                    Calculator.setSign(mathElements[1].charAt(0));
-                    if (!(checkIntPosNum(num1) && checkIntPosNum(num2))) {
-                        System.out.println("Введенные числа должны быть положительными и целыми. Попробуйте еще раз.");
-                    }
-                } while (!(checkIntPosNum(num1) && checkIntPosNum(num2)));
-                Calculator.setNum1((int) num1);
-                Calculator.setNum2((int) num2);
-            System.out.print("= " + Calculator.calculate() + "\n");
+                System.out.println("Введите математическое выражение: ");
+                String mathAction = scanner.nextLine();
+            System.out.print("= " + Calculator.calculate(mathAction) + "\n");
             } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 System.out.println("Неверный формат выражения! [Вводите в формате X + Y]");
                 continue;
             } catch (IllegalArgumentException e) {
                 System.out.println("Несуществующая операция! [доступные операции + - / * ^]");
                 continue;
+            } catch (InvalidValuesException e) {
+                System.out.println("Введенные числа должны быть положительными и целыми. Попробуйте еще раз.");
+                continue;
             }
+
             do {
                 System.out.println("Хотите продолжить вычисление? [yes / no] " );
                 option = scanner.nextLine();
@@ -45,9 +36,4 @@ public class CalculatorTest {
     private static boolean checkContinue(String option) {
         return (!option.equals("yes") && !option.equals("no"));
     }
-
-    private static boolean checkIntPosNum(float num) {
-        return (num % 1 == 0 && num >= 0);
-    }
 }
-
